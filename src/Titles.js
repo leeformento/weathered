38e06a1608c7154b4c3be080262c40b25057c737
leeformento <39426065+leeformento@users.noreply.github.com>
2018-09-29 14:13:31 -0400

    Add files via upload

---
+++ src/Titles.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// No state
+// class Titles extends React.Component {
+//     render() {
+//         return  (
+//             <div>
+//                 <h1>Weather Finder</h1>
+//                 <p> Find out</p>
+//             </div>
+//         );
+//     }
+// }
+
+
+const Titles = () => (
+    <div>
+    <h1 className ="title-container__title">WEATHERED</h1>
+    <h3 className ="title-container__subtitle"> </h3> 
+</div>
+);
+
+export default Titles;
